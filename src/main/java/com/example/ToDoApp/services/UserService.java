8ef08e4eb303db0980df79a30d943e283e34e1fc
POOLSH <kir180920032003@gmail.com>
2025-02-07 11:13:48 +0300

    Create auth logic

---
+++ src/main/java/com/example/ToDoApp/services/UserService.java
@@ -0,0 +1,50 @@
+package com.example.ToDoApp.services;
+
+import com.example.ToDoApp.models.Role;
+import com.example.ToDoApp.models.User;
+import com.example.ToDoApp.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User createUser(User user) {
+        if(userRepository.existsByUsername(user.getUsername())) {
+            throw new RuntimeException("Username already exists");
+        }
+        if(userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+        return saveUser(user);
+    }
+
+    public User getByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+    }
+
+    public UserDetailsService userDetailsService() {
+        return this::getByUsername;
+    }
+
+    public User getCurrentUser() {
+        var userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getByUsername(userName);
+    }
+
+    @Deprecated
+    public void getAdmin(){
+        var user=getCurrentUser();
+        user.setRole(Role.ROLE_ADMIN);
+        saveUser(user);
+    }
+}
